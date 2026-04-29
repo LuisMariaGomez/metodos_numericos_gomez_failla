f0681bacb76a71f429b0dcd7cf1c9e90b2cfd94e
Luis María Gómez <125579465+LuisMariaGomez@users.noreply.github.com>
2026-04-29 18:06:45 -0300

    gauss-jordan

--- Metodo Numerico/backend/unidad2/main.py
+++ Metodo Numerico/backend/unidad2/main.py
@@ -1,13 +1,59 @@
-matriz = [[5, 1, 4.01015],[-1.301525, 0.25, 1.10075],[3.751125, 0.801216, 3.002028]]
+matriz = [
+    [5, 1, 4.01015],
+    [-1.301525, 0.25, 1.10075],
+    [3.751125, 0.801216, 3.002028],
+]
 valores_independientes = [1, 0.225, 0.75]
 
 n = len(matriz)
-for i in range(n):
-    for j in range(n):
-        if (i==j):
-            valor_pivote =  matriz[i][j]
-            for k in range(1,n):
-                matriz[i][k] = matriz[i][k] / valor_pivote
-            matriz[i][j]=1
 
-print(matriz)
+print()
+for i in range(n):
+    if matriz[i][i] == 0:
+        for fila in range(i + 1, n):
+            if matriz[fila][i] != 0:
+                matriz[i], matriz[fila] = matriz[fila], matriz[i]
+                valores_independientes[i], valores_independientes[fila] = (
+                    valores_independientes[fila],
+                    valores_independientes[i],
+                )
+                break
+        else:
+            raise ValueError("No se puede continuar: pivote nulo.")
+
+    print("Hacer 1 al pivote y dividir el resto de la fila")
+    valor_pivote = matriz[i][i]
+    print(valor_pivote)
+
+    for k in range(n):
+        print(f"se hace {matriz[i][k]} / {valor_pivote}")
+        matriz[i][k] = matriz[i][k] / valor_pivote
+        print(f"dando {matriz[i][k]}")
+
+    print(
+        f"se hace valor independiente: {valores_independientes[i]} / {valor_pivote}"
+    )
+    valores_independientes[i] = valores_independientes[i] / valor_pivote
+    print(f"dando: {valores_independientes[i]}")
+    print(f"la fila queda {matriz[i]} | {valores_independientes[i]}")
+    print()
+
+    print("Reducir el resto de filas")
+    for l in range(n):
+        if l == i:
+            continue
+
+        valor_a_0 = matriz[l][i]
+        print(f"valor a hacer 0: {valor_a_0}\n")
+
+        for m in range(n):
+            matriz[l][m] = matriz[l][m] - valor_a_0 * matriz[i][m]
+
+        valores_independientes[l] = (
+            valores_independientes[l] - valor_a_0 * valores_independientes[i]
+        )
+
+
+print("Validaciones")
+for i in range(n):
+    print(f"{matriz[i]} | {valores_independientes[i]}")
