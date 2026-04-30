07146a3c3beab86a28604db503ad16fb6d80dd44
Luis Maria Gomez <chuni12gomez@gmail.com>
2026-04-30 13:12:15 -0300

    comentarios

--- Metodo Numerico/backend/unidad2/main.py
+++ Metodo Numerico/backend/unidad2/main.py
@@ -8,7 +8,9 @@ valores_independientes = [1, 0.225, 0.75]
 n = len(matriz)
 
 print()
+# para recorresr las filas
 for i in range(n):
+    #Caso de qe sea 0 el pivote
     if matriz[i][i] == 0:
         for fila in range(i + 1, n):
             if matriz[fila][i] != 0:
@@ -18,13 +20,14 @@ for i in range(n):
                     valores_independientes[i],
                 )
                 break
-        else:
-            raise ValueError("No se puede continuar: pivote nulo.")
+            else:
+                raise ValueError("No se puede continuar: pivote nulo.")
 
-    print("Hacer 1 al pivote y dividir el resto de la fila")
+    print("\n Hacer 1 al pivote y dividir el resto de la fila")
     valor_pivote = matriz[i][i]
     print(valor_pivote)
-
+    
+    # para recorrer las columnas
     for k in range(n):
         print(f"se hace {matriz[i][k]} / {valor_pivote}")
         matriz[i][k] = matriz[i][k] / valor_pivote
@@ -35,18 +38,21 @@ for i in range(n):
     )
     valores_independientes[i] = valores_independientes[i] / valor_pivote
     print(f"dando: {valores_independientes[i]}")
-    print(f"la fila queda {matriz[i]} | {valores_independientes[i]}")
-    print()
+    print(f"la fila queda {matriz[i]} | {valores_independientes[i]} \n")
 
     print("Reducir el resto de filas")
+    # para recorrer las filas sin tocar los indices que usamos antes
     for l in range(n):
+        # si es la fila del pivote no se hace nada
         if l == i:
             continue
 
         valor_a_0 = matriz[l][i]
-        print(f"valor a hacer 0: {valor_a_0}\n")
+        print(f"\n valor a hacer 0: {valor_a_0}\n")
 
+        # para recorrer las columnas y hacer 0 el valor debajo del pivote y reducir el resto de la fila
         for m in range(n):
+            print(f"se hace {matriz[l][m]} - {valor_a_0} * {matriz[i][m]}, dando {matriz[l][m] - valor_a_0 * matriz[i][m]}")
             matriz[l][m] = matriz[l][m] - valor_a_0 * matriz[i][m]
 
         valores_independientes[l] = (
