77e4de023d841863d529e5d749b65c02b12de6ef
Luis Maria Gomez <chuni12gomez@gmail.com>
2026-05-02 13:32:06 -0300

    jordan_y_seidel

---
+++ Metodo Numerico/backend/unidad2/gauss-jorndan.py
@@ -0,0 +1,47 @@
+"""
+valores de ingreso:
+matriz: matriz de coeficientes
+valores_independientes: vector de terminos independientes
+"""
+def gauss_jordan(matriz, valores_independientes):
+    n = len(matriz)
+    # para recorrer las filas
+    for i in range(n):
+        #Caso de qe sea 0 el pivote y haya que hacer un intercambio de filas
+        if matriz[i][i] == 0:
+            for fila in range(i + 1, n):
+                if matriz[fila][i] != 0:
+                    matriz[i], matriz[fila] = matriz[fila], matriz[i]
+                    valores_independientes[i], valores_independientes[fila] = (
+                        valores_independientes[fila],
+                        valores_independientes[i],
+                    )
+                    break
+                else:
+                    raise ValueError("No se puede continuar: pivote nulo.")
+
+        valor_pivote = matriz[i][i]
+        
+        # para recorrer las columnas
+        for k in range(n):
+            matriz[i][k] = matriz[i][k] / valor_pivote
+
+        valores_independientes[i] = valores_independientes[i] / valor_pivote
+
+        # para recorrer las filas sin tocar los indices que usamos antes
+        for l in range(n):
+            # si es la fila del pivote no se hace nada
+            if l == i:
+                continue
+
+            valor_a_0 = matriz[l][i]
+
+            # para recorrer las columnas y hacer 0 el valor debajo del pivote y reducir el resto de la fila
+            for m in range(n):
+                matriz[l][m] = matriz[l][m] - valor_a_0 * matriz[i][m]
+
+            valores_independientes[l] = (
+                valores_independientes[l] - valor_a_0 * valores_independientes[i]
+            )
+
+    return valores_independientes
---
+++ Metodo Numerico/backend/unidad2/gauss-seidel.py
@@ -0,0 +1,43 @@
+"""
+valores de ingreso:
+A: matriz de coeficientes
+b: vector de términos independientes
+tol: tolerancia para la convergencia
+max_iter: número máximo de iteraciones permitidas
+"""
+def gauss_seidel(A, b, tol=1e-4, max_iteraciones=100):
+    n = len(b)
+    x = [0.0] * n  # vector inicial en 0
+
+    for iteraciones in range(max_iteraciones):
+        x_old = x.copy() # el copy es porque despues si modificamos x, x_old no se modifica, si hariamos x_old = x, entonces x_old y x apuntarian a la misma lista, y al modificar x, x_old tambien se modificaria
+        for i in range(n):
+            suma = 0.0
+            for j in range(n):
+                if i != j:
+                    suma += A[i][j] * x[j]
+
+            x[i] = (b[i] - suma) / A[i][i]
+
+        # cálculo del error (norma infinito, pero casera)
+        error = 0.0
+        for i in range(n):
+            diff = abs(x[i] - x_old[i])
+            if diff > error:
+                error = diff
+
+        if error < tol:
+            print(f"Convergió en {iteraciones+1} iteraciones")
+            return x
+
+    print("No convergió en el máximo de iteraciones")
+
+
+# Para probar
+# A = [[4, -1, 0, 0],
+#      [-1, 4, -1, 0],
+#      [0, -1, 4, -1],
+#      [0, 0, -1, 3]]
+# b = [15, 10, 10, 10]
+# resultado = gauss_seidel(A, b)
+# print("La solución es:", resultado)
